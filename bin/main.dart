cbde561efec1e9b6e0a6a7f86f82a0f928d36b3c
Boburbek6010 <boburbekkhusanboev@gmail.com>
2024-03-04 22:07:32 +0500

    Boburbek, 4-commit, sms

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,30 @@
 import 'dart:io';
+import 'dart:math';
 
-import 'package:learn_structure_g10/data/language_list.dart';
-import 'package:translator/translator.dart';
+import 'package:learn_structure_g10/models/send_sms_model.dart';
+import 'package:learn_structure_g10/services/network_service.dart';
 
-void main()async{
-  DateTime dateTime1 = DateTime.now();
-  DateTime dateTime2 = DateTime.now();
-  Set set = {};
-  set.add(dateTime1);
-  set.add(dateTime2);
-  print(set);
+void main() async{
+
+  print("Qaysi raqamga jonatmoqchisiz?");
+  // int number = int.parse(stdin.readLineSync()!);
+
+  print("Text:");
+
+  int random = Random().nextInt(8999)+1000;
+
+  String text = stdin.readLineSync()!;
+
+  String message = "$text => $random";
+
+  SendMassageModel sendMassageModel = SendMassageModel(mobilePhone: "998998138472", message: message, from: "1111");
+
+  String? result = await NetworkService.postData(NetworkService.baseUrl, NetworkService.apiSendSMS, sendMassageModel.toJson());
+
+  print(result);
+
+
 }
+
+
+
--- lib/services/network_service.dart
+++ lib/services/network_service.dart
@@ -3,23 +3,27 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 
+
 class NetworkService {
-  static const String baseUrl = "generativelanguage.googleapis.com";
-  static const String apiGetGeminiText = "/v1beta/models/gemini-pro:generateContent";
+  static const String baseUrl = "notify.eskiz.uz";
+  static const String apiSendSMS = "/api/message/sms/send";
 
   static const Map<String, String> headers = {
-    "Content-Type": "application/json"
+  "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTIwMzM0NTAsImlhdCI6MTcwOTQ0MTQ1MCwicm9sZSI6InRlc3QiLCJzaWduIjoiNjMxZTExYmE1MmRiZTQwZmFmZGExZWVhZWIwYWZkNmNmZDA1ZjhiMTllNjE1ZGU3MzU5OTNkNGYwNWI2OGJmNCIsInN1YiI6IjY2MTQifQ.vG_U97RI4_xGQ2tvZHKpPx8v8ZA80bT5ZjUZk-Iq31Q'
   };
 
-  static Future<String?> postData(String baseUrl, String api, Map<String, dynamic> body, Map<String, dynamic> param) async {
-    Uri url = Uri.https(baseUrl, api, param);
-    Response response = await http.post(url, body: jsonEncode(body), headers: headers);
+  static Future<String?> postData(String baseUrl, String api, Map<String, dynamic> body) async {
+    Uri url = Uri.http(baseUrl, api);
+    Response response = await http.post(url, body: body, headers: headers);
     if (response.statusCode == 200 || response.statusCode == 201) {
       return response.body;
     } else {
       return null;
     }
   }
+
+
+
 
 
   static Map<String, dynamic> paramGeminiText() => <String, dynamic>{
